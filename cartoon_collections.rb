767906e8bd5a4f608bbdfd18c25895732317f7a0
Jack Yeung <jyeung.se@gmail.com>
2018-06-19 04:30:03 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.collect do |element|
+  planeteer_calls.map do |element|
     element.upcase
   end
 end
